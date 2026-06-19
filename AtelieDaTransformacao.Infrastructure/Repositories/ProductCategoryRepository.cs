08692c8b63f49078d948fb41eaaf911dc661bcdc
Igor <igorblucaxavixus24@gamil.com>
2026-06-19 15:16:24 -0300

    ATT INFRAESTRUTURA COMPLETA COM REPOSITORIOS

--- AtelieDaTransformacao.Infrastructure/Repositories/ProductCategoryRepository.cs
+++ AtelieDaTransformacao.Infrastructure/Repositories/ProductCategoryRepository.cs
@@ -1,10 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using AtelieDaTransformacao.Domain.Entities;
+using AtelieDaTransformacao.Domain.Interfaces;
+using AtelieDaTransformacao.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace AtelieDaTransformacao.Infrastructure.Repositories
 {
-    internal class ProductCategoryRepository
+    public class ProductCategoryRepository : IProductCategoryRepository
     {
+        private readonly AtelieDaTransformacaoDbContext _context;
+
+        public ProductCategoryRepository(AtelieDaTransformacaoDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<ProductCategory>> GetAllAsync()
+        {
+            return await _context.ProductCategories
+                .Include(c => c.Product)
+                .ToListAsync();
+        }
+
+        public async Task<ProductCategory?> GetByIdAsync(int id)
+        {
+            return await _context.ProductCategories
+                .Include(c => c.Product)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task AddAsync(ProductCategory category)
+        {
+            await _context.ProductCategories.AddAsync(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(ProductCategory category)
+        {
+            _context.ProductCategories.Update(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var category = await _context.ProductCategories.FindAsync(id);
+
+            if (category != null)
+            {
+                _context.ProductCategories.Remove(category);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<int> CountAsync()
+        {
+            return await _context.ProductCategories.CountAsync();
+        }
     }
 }
--- AtelieDaTransformacao.Infrastructure/Repositories/ProductRepository.cs
+++ AtelieDaTransformacao.Infrastructure/Repositories/ProductRepository.cs
@@ -1,10 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using AtelieDaTransformacao.Domain.Entities;
+using AtelieDaTransformacao.Domain.Interfaces;
+using AtelieDaTransformacao.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace AtelieDaTransformacao.Infrastructure.Repositories
 {
-    internal class ProductRepository
+    public class ProductRepository : IProductRepository
     {
+        private readonly AtelieDaTransformacaoDbContext _context;
+
+        public ProductRepository(AtelieDaTransformacaoDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            return await _context.Products
+                .Include(p => p.Category)
+                .ToListAsync();
+        }
+
+        public async Task<Product?> GetByIdAsync(int id)
+        {
+            return await _context.Products
+                .Include(p => p.Category)
+                .FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<IEnumerable<Product>> GetFeaturedAsync()
+        {
+            return await _context.Products
+                .Include(p => p.Category)
+                .Where(p => p.IsFeatured)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetByCategoryAsync(int categoryId)
+        {
+            return await _context.Products
+                .Include(p => p.Category)
+                .Where(p => p.CategoryId == categoryId)
+                .ToListAsync();
+        }
+
+        public async Task AddAsync(Product product)
+        {
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<int> CountAsync()
+        {
+            return await _context.Products.CountAsync();
+        }
     }
 }
