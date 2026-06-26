105c7fbe2d6864f61d80e5589fa557c75f5d2c91
renan.fribeiro2 <renan.fribeiro2@SMP0711473W11-1>
2026-06-26 16:22:09 -0300

    Att Painel

--- AtelieDaTransformacao.UI/wwwroot/js/site.js
+++ AtelieDaTransformacao.UI/wwwroot/js/site.js
@@ -1,12 +1,13 @@
-﻿document.addEventListener("DOMContentLoaded", function () {
+﻿
+        document.addEventListener("DOMContentLoaded", function () {
     // Adiciona feedback visual imediato nos inputs ativos
     const inputs = document.querySelectorAll(".form-control, .form-select");
 
     inputs.forEach(input => {
-        input.addEventListener("focus", () => {
-            // Aplica a classe de controle diretamente no próprio input
-            input.classList.add("form-active");
-        });
+            input.addEventListener("focus", () => {
+                // Aplica a classe de controle diretamente no próprio input
+                input.classList.add("form-active");
+            });
 
         input.addEventListener("blur", () => {
             // Remove o efeito ao sair do campo
